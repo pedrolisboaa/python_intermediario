a4deec87e6e05e64d05d4df20fdb86820b9ec2f4
Pedro Lisboa <47804942+pedrolisboaa@users.noreply.github.com>
2021-08-24 20:34:37 -0300

    Create desafios_2.py

---
+++ 1-Funções/desafios_2.py
@@ -0,0 +1,29 @@
+"""
+Desafio 2 Funções
+"""
+
+# Desafio 1
+def func_one(funcao):
+    return funcao
+
+
+def func_two():
+    return 'Pedro Lisboa'
+
+
+print(func_one(func_two()))
+
+
+#Desafio 2
+
+def funcao_um(funcao, *args, **kwargs):
+    return funcao(*args, **kwargs)
+
+def fala_oi(nome):
+    return f'oi {nome}.'
+
+def saudacao(nome, saudacao):
+    return f'{saudacao} {nome}.'
+
+executando = funcao_um(fala_oi, 'Pedro')
+print(executando)
