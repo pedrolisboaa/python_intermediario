e687d6787313e625a390091bd86e5a8e7cf46e47
Pedro Lisboa <47804942+pedrolisboaa@users.noreply.github.com>
2021-08-27 20:09:05 -0300

    Update 12-try-excpet.py

--- 12-try-excpet.py
+++ 12-try-excpet.py
@@ -11,4 +11,27 @@ else:
   print('Seu código foi executado com sucesso')
 finally:
   print('Finalmente')
+  
+  
+print('----------')
+
+def converte_numero(valor):
+  try:
+    valor = int(valor)
+    return valor
+  except ValueError:
+    try: 
+      valor = float(valor)
+      return valor
+    except ValueError:
+      pass
+  
+
+
+
+numero = converte_numero(input('Digite um número: '))
+if numero is not None:
+  print(numero + 5)
+else:
+  print('Isso não é um número')
 
